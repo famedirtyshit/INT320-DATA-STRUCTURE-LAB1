bd135c96182d227d593d4bf45f316832e46fe22c
int320 <famedirtyshit@gmail.com>
2020-01-24 12:09:00 +0700

    [UPDATE] add test case

--- JavaApplication1.java
+++ JavaApplication1.java
@@ -16,10 +16,12 @@ import java.util.Random;
 public class JavaApplication1 {
 
     private static int N = 100_000;
+    private static int N2 = 10_000;
     private static Random random = new Random();
     private static Integer[] a1 = new Integer[N];
     private static Integer[] a2 = new Integer[N];
-    private static Integer[] a3 = new Integer[N];
+    private static Integer[] a3 = new Integer[N2];
+    private static Integer[] a4 = new Integer[N2];
 
     /**
      * @param args the command line arguments
@@ -49,35 +51,37 @@ public class JavaApplication1 {
         for (int i = 0; i < a1.length; i++) {
             a1[i] = random.nextInt(N);
         }
+        for (int i = 0; i < a3.length; i++) {
+            a3[i] = random.nextInt(N2);
+        }
         System.arraycopy(a1, 0, a2, 0, a1.length);
-        System.arraycopy(a1, 0, a3, 0, a1.length);
+        System.arraycopy(a3, 0, a4, 0, a3.length);
     }
 
-    public static void main(String[] args) {
-//        Date date = new Date(System.currentTimeMillis());
-//         int[]     array  = {12, 11, 15, 10, 9, 1, 2, 3, 13, 14, 4, 5, 6, 7, 8};
-//        JavaApplication1 sorter = new JavaApplication1();
-//        sorter.quickSort(array, 0, array.length - 1);
-//        System.out.println(java.util.Arrays.toString(array));
-//        
+    public static void main(String[] args) {     
         init();
-        //Arrays.sort(a1);
         System.out.println("compare insertion sort O(n!) with array sort O(n log n)");
         JavaApplication1 sorter = new JavaApplication1();
-        long beginInsertionSort = System.currentTimeMillis();
+        
+        long beginInsertionSort1 = System.currentTimeMillis();
         sorter.insertionSort(a1);
-        long usedTimeInsertion = System.currentTimeMillis()-beginInsertionSort;
-        //System.out.println("selection sort time" + usedTimeInsertion);
-        //System.out.println(java.util.Arrays.toString(a1));
-        long beginArraySort = System.currentTimeMillis();
+        long usedTimeInsertion1 = System.currentTimeMillis()-beginInsertionSort1;
+        long beginArraySort1 = System.currentTimeMillis();
         Arrays.sort(a2);
-        long usedTimeArraySort = System.currentTimeMillis()-beginArraySort;
-        //System.out.println("Arrays.sort() time" + usedTimeArraySort);
-        //System.out.println(java.util.Arrays.toString(a2));
-        System.out.println("**********************************************");
+        long usedTimeArraySort1 = System.currentTimeMillis()-beginArraySort1;
+        
+        long beginInsertionSort2 = System.currentTimeMillis();
+        sorter.insertionSort(a3);
+        long usedTimeInsertion2 = System.currentTimeMillis()-beginInsertionSort2;
+        long beginArraySort2 = System.currentTimeMillis();
+        Arrays.sort(a4);
+        long usedTimeArraySort2 = System.currentTimeMillis()-beginArraySort2;
+        
+        System.out.println("\n**********************************************");
         System.out.println("input\t"+"Your Program\t"+"Arrays.sort");
         System.out.println("**********************************************");
-        System.out.println(N+"\t"+usedTimeInsertion+" ms"+"\t"+usedTimeArraySort+" ms");
+        System.out.println(N+"\t"+usedTimeInsertion1+" ms"+"\t"+usedTimeArraySort1+" ms\n");
+        System.out.println(N2+"\t"+usedTimeInsertion2+" ms"+"\t\t"+usedTimeArraySort2+" ms\n");
         System.out.println("credit https://big-o.io/algorithms/comparison/insertion-sort/");
     }
 
